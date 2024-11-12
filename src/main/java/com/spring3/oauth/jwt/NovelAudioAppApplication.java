d987c0bd25a93ef0a89fd67dd35ac9324f4005fe
Duonggtt <114550227+Duonggtt@users.noreply.github.com>
2024-11-12 07:24:02 +0700

    swg-config

--- src/main/java/com/spring3/oauth/jwt/NovelAudioAppApplication.java
+++ src/main/java/com/spring3/oauth/jwt/NovelAudioAppApplication.java
@@ -12,7 +12,7 @@ import org.springframework.web.client.RestTemplate;
 
 @EnableJpaRepositories(repositoryBaseClass = RefreshableCRUDRepositoryImpl.class)
 @SpringBootApplication
-//@PropertySource("file:.env")
+@PropertySource("file:.env")
 public class NovelAudioAppApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
