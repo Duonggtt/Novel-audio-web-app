d11a0ce8fb88af23fe7ba1907f46e378411546dc
Duonggtt <114550227+Duonggtt@users.noreply.github.com>
2024-12-10 22:24:34 +0700

    fix-cfg

--- src/main/java/com/spring3/oauth/jwt/configurations/WebSocketConfig.java
+++ src/main/java/com/spring3/oauth/jwt/configurations/WebSocketConfig.java
@@ -18,6 +18,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS(); // Endpoint WebSocket
+        registry.addEndpoint("/ws").setAllowedOrigins("14.225.207.58:9898").withSockJS(); // Endpoint WebSocket
     }
 }
