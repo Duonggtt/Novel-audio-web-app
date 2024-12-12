99ff1a04b1b3f45adef759fb5254d2fed685f8b1
Duonggtt <114550227+Duonggtt@users.noreply.github.com>
2024-12-12 21:21:14 +0700

    rn-class

--- src/main/java/com/spring3/oauth/jwt/repositories/ReportRepository.java
+++ src/main/java/com/spring3/oauth/jwt/repositories/ReportRepository.java
@@ -1,14 +1,14 @@
 package com.spring3.oauth.jwt.repositories;
 
-import com.spring3.oauth.jwt.entity.Report;
+import com.spring3.oauth.jwt.entity.UserActivityReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ReportRepository extends JpaRepository<Report, Long>{
+public interface ReportRepository extends JpaRepository<UserActivityReport, Long>{
 
-    List<Report> findByDate(String date); // Lấy báo cáo theo ngày
-    List<Report> findByDateAndApiPath(String date, String apiPath); // Lấy báo cáo theo ngày và apiPath
+    List<UserActivityReport> findByDate(String date); // Lấy báo cáo theo ngày
+    List<UserActivityReport> findByDateAndApiPath(String date, String apiPath); // Lấy báo cáo theo ngày và apiPath
 }
