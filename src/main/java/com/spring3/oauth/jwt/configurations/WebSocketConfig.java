f4de82ea436c98f46a39f076946938170d92e43d
Duonggtt <114550227+Duonggtt@users.noreply.github.com>
2024-12-17 13:29:01 +0700

    .

--- src/main/java/com/spring3/oauth/jwt/configurations/WebSocketConfig.java
+++ src/main/java/com/spring3/oauth/jwt/configurations/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package com.spring3.oauth.jwt.configurations;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -10,6 +11,7 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic", "/chat"); // Topic cho client đăng ký nhận thông báo
@@ -18,6 +20,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("localhost:9898").withSockJS(); // Endpoint WebSocket
+        registry.addEndpoint("/ws").setAllowedOrigins("14.225.207.58:9898").withSockJS(); // Endpoint WebSocket
     }
 }
