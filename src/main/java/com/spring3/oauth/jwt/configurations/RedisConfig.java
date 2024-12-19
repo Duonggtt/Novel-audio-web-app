cc0616699dabb26139aef09ba8e379e14ad7572c
Huy Chiến <chienhi912@gmail.com>
2024-12-19 21:47:18 +0700

    Redis conn use env

--- src/main/java/com/spring3/oauth/jwt/configurations/RedisConfig.java
+++ src/main/java/com/spring3/oauth/jwt/configurations/RedisConfig.java
@@ -6,16 +6,28 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class RedisConfig {
-    // Cấu hình kết nối Redis (Jedis)
+    @Value("${REDIS_HOST}")
+    private String redisHost;
+    
+    @Value("${REDIS_PORT}")
+    private int redisPort;
+    
+    @Value("${REDIS_PASSWORD:#{null}}")
+    private String redisPassword;
+
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName("localhost");
-        factory.setPort(6379);
-        factory.setUsePool(true); // Dùng pool kết nối
+        factory.setHostName(redisHost);
+        factory.setPort(redisPort);
+        if (redisPassword != null && !redisPassword.trim().isEmpty()) {
+            factory.setPassword(redisPassword);
+        }
+        factory.setUsePool(true);
         return factory;
     }
 
