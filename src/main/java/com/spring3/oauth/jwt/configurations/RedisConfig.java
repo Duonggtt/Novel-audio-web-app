1499d85786768de3cb0207718e412553e8fa7f77
Huy Chiến <chienhi912@gmail.com>
2024-12-19 22:28:30 +0700

    Update SLL with RedisConfig

--- src/main/java/com/spring3/oauth/jwt/configurations/RedisConfig.java
+++ src/main/java/com/spring3/oauth/jwt/configurations/RedisConfig.java
@@ -2,6 +2,8 @@ package com.spring3.oauth.jwt.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
@@ -21,23 +23,24 @@ public class RedisConfig {
 
     @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory factory = new JedisConnectionFactory();
-        factory.setHostName(redisHost);
-        factory.setPort(redisPort);
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(redisHost);
+        redisConfig.setPort(redisPort);
         if (redisPassword != null && !redisPassword.trim().isEmpty()) {
-            factory.setPassword(redisPassword);
+            redisConfig.setPassword(redisPassword);
         }
-        factory.setUsePool(true);
-        return factory;
+
+        JedisClientConfiguration.JedisClientConfigurationBuilder builder = JedisClientConfiguration.builder();
+        builder.useSsl();
+        
+        return new JedisConnectionFactory(redisConfig, builder.build());
     }
 
-    // Cấu hình RedisTemplate
     @Bean
     public RedisTemplate<String, Object> redisTemplate() {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(jedisConnectionFactory());
 
-        // Sử dụng Jackson để serialize/deserialize Object thành JSON
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
